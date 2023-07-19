c05792552ca3cbccbdfeed71992ceb196b1ce21f
Paul <llanquepol@gmail.com>
2023-07-18 21:42:54 -0500

    add validations and associations of the models

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,7 @@
 class Department < ApplicationRecord
+  # Validations
+  validates :name, presence: true, uniqueness: true
+
+  # Associations
+  has_many :employees, dependent: :destroy
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ApplicationRecord
+  # Validations
+  validates :name, presence: true, uniqueness: true,
+  validates :birth_date, comparison: { less_than: 18.years.ago, 
+                                        message: "must be at least 18 years old" }
+
+  # Associations
   belongs_to :department
 end
