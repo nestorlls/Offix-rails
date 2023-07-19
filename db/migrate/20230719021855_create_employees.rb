24a62688c88edf0f9290b328615afc17faa3b1c7
Paul <llanquepol@gmail.com>
2023-07-18 21:26:22 -0500

    add migrations and schema

---
+++ db/migrate/20230719021855_create_employees.rb
@@ -0,0 +1,14 @@
+class CreateEmployees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :nationality
+      t.string :role
+      t.date :birth_date
+      t.references :department, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :employees, :name, unique: true
+  end
+end
